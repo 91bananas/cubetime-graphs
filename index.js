599fd7df3cd842a3f75d3d978047e3fb5046b395
david cushman <davidrcushman@gmail.com>
2017-02-24 15:21:07 -0600

    first

---
+++ index.js
@@ -0,0 +1,76 @@
+var _ = require('underscore');
+var c3 = require('c3');
+var $ = require('jquery');
+
+//line chart over time
+var parsed = _.map(data, function (time, index) {
+    return time[0][1]/1000;
+    return list;
+});
+parsed.unshift('first');
+console.log('line chart data');
+console.log(parsed);
+
+/*{
+    "keys":{
+        "value":[
+            "Existing Customer",
+            "Other",
+            "Referral",
+            "Inbound",
+            "SDRs",
+            "Employee Referral"
+        ]
+    },
+    "json":[
+        {"Existing Customer":102},
+        {"Other":94},
+        {"Referral":165},
+        {"Inbound":134},
+        {"SDRs":133},
+        {"Employee Referral":21}
+    ],
+    "type":"donut"
+}*/
+
+var chart = c3.generate({
+    data: {
+        // x: 'x',
+        columns: [
+            parsed
+        ],
+        type: 'bar',
+    },
+    // axis: {
+    //     x: {
+    //         // type: 'category',
+    //         tick: {
+    //             format: function (x) {
+    //                 return x;
+    //             }
+    //         },
+    //         height: 40,
+    //     },
+    //     y: {
+    //         min: 0,
+    //         max: 1,
+    //         padding: {
+    //             bottom: 0,
+    //             top: 4
+    //         },
+    //         tick: {
+    //             format: function (y) {
+    //                 return y;
+    //             }
+    //         },
+    //     }
+    // },
+    tooltip: {
+        format: {
+            name: function (name) {
+                return templateHelpers.titleify(name);
+            }
+        }
+    }
+});
+$('.chart').empty().append(chart.element);
---
+++ run.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var _ = require('underscore');
+var watchify = require('watchify');
+var mkdirp = require('mkdirp'),
+    // colors = require('colors');
+chalk = require('chalk');
+
+var compileUnderscore = require('node-underscorify').transform({
+    templateSettings: {
+        evaluate: /\{\{#(.+?)\}\}/g,
+        interpolate: /\{\{=(.+?)\}\}/g,
+        escape: /\{\{(?!#|=)(.+?)\}\}/g
+    }
+});
+var files = [
+    ['index.js', 'build.js'],
+];
+
+var watching = false;
+var initConfig = {
+    entries: _.pluck(files, 0),
+};
+
+if (process.argv[2] == '--watch') {
+    watching = true;
+    initConfig.cache = {};
+    initConfig.packageCache = {};
+    initConfig.plugin = [watchify];
+}
+
+var b = require('browserify')(initConfig);
+b.plugin('factor-bundle', {
+    outputs: _.pluck(files, 1)
+});
+b.transform('lessify');
+b.transform(compileUnderscore);
+
+b.on('update', doTheThing);
+doTheThing();
+function doTheThing(e) {
+    console.log(chalk.cyan('Starting build!'));
+
+    mkdirp('features/bundles/', function (err) {
+        if (err) {
+            console.log(chalk.bgRed.white(err));
+        } else {
+            b.bundle(function (err, buf) {
+                if (buf) {
+                    var message = chalk.dim('/static/reloaded/') + ' Bundles created.';
+                    if (watching) {
+                        message += chalk.yellow(' Watchifying...');
+                    }
+                    console.log(message);
+                }
+            })
+            .on('error', function (err) {
+                console.log(chalk.bgRed.white(err.message));
+            }).pipe(fs.createWriteStream('features/bundles/common.js'));
+        }
+    });
+}
